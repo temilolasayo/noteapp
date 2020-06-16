d8bf22936681cece8f928b80f0aa02459f125cf0
temilolasayo <alajikitemilola@gmail.com>
2020-06-16 10:02:29 +0100

    New changes to server folder

---
+++ build/precache-manifest.b9c077418c3de5ca5ac6d6f20b77695b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93bb3b52797eb5aa183d4f9b1e776d26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0818083d6b727506a5c",
+    "url": "/static/css/2.1d2230e4.chunk.css"
+  },
+  {
+    "revision": "92edd1d9f2dc55fa9b9f",
+    "url": "/static/css/main.5270437e.chunk.css"
+  },
+  {
+    "revision": "e0818083d6b727506a5c",
+    "url": "/static/js/2.5803b3bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5803b3bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92edd1d9f2dc55fa9b9f",
+    "url": "/static/js/main.a28da6f7.chunk.js"
+  },
+  {
+    "revision": "3bf4bf2063b392eeff5c",
+    "url": "/static/js/runtime-main.a11edf94.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
